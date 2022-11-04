4a1f06b633973b3ead2cbca5a7adc4a15ac1fe84
YuraJian <ur.duimja@gmail.com>
2022-11-04 20:28:38 +0200

    some script for js

---
+++ lesson2/cw2/data_types/script.js
@@ -0,0 +1,12 @@
+console.log(typeof 123);
+console.log(typeof 'test');
+
+let example;
+console.log('declare', typeof example);
+
+example = 10;
+console.log('init with number', typeof example);
+
+example = 'test';
+console.log('init with string', typeof example);
+
