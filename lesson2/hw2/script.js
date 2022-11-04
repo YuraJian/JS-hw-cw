6c6a2398b86a41172c2df143349a0510e44f7c66
YuraJian <ur.duimja@gmail.com>
2022-11-04 20:53:58 +0200

    ready homework with script.js

---
+++ lesson2/hw2/script.js
@@ -0,0 +1,22 @@
+// 1.
+let userName = 'Yura';
+let userSurname = 'Demianchuk';
+let userAge = 22;
+
+// 2.
+let newUser;
+let shoppingList;
+let isUserOnline;
+let employeeSalary;
+let mouseCoords;
+
+// 3.
+console.log('Hello World!')
+
+let greeting = 'Hello World!';
+console.log(greeting);
+
+
+console.log('userName', userName);
+
+console.log(`Hello ${userName} ${userSurname}. How old are you? I'm ${userAge}`);
