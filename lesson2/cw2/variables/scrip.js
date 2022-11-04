abb36fda0775566ab0206ee89f8b02faeae78994
YuraJian <ur.duimja@gmail.com>
2022-11-04 19:59:40 +0200

    script for variables for study

---
+++ lesson2/cw2/variables/scrip.js
@@ -0,0 +1,65 @@
+// Hello World
+
+let firstVariable;
+    firstVariable = 'Hello World';
+
+console.log(firstVariable);
+
+let userName = 'Yura';
+console.log(userName);
+
+let width, height, length;
+
+let maxSpeed = 200;
+    minSpeed = 30;
+
+let button = 'button';
+let input = 'input';
+let grid;
+
+let myHobby = 'music';
+// let myHobby = 'code';
+
+console.log(myHobby);
+
+let childAge = 12;
+
+childAge = 13;
+
+console.log(childAge);
+
+let rectangleWidth = 200;
+let rectangleHeight = rectangleWidth;
+
+let user;
+let userSurname;
+let info1;
+let $;
+let _;
+
+// let 1info; Невірний запис змінної. Змінна не може починатись з числа!!!
+
+const clientNickname = 'useruser';
+// clientNickname = 'user';
+
+const PI = 3.14;
+const BLACK_COLOR = '#0000';
+
+oldBoy = 60;
+console.log(oldBoy);
+
+var oldBoy = 40;
+oldBoy = 70;
+
+console.log(oldBoy);
+
+if (true) {
+    var a = 20;
+}
+
+console.log(a);
+ 
+// function example() {
+//     var c = 1;
+// }
+// console.log(c); it's error
