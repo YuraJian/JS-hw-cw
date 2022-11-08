9fa1fc31f2e0dd15f1b81b133e2fa6b57a77d51e
YuraJian <ur.duimja@gmail.com>
2022-11-09 00:57:36 +0200

    complete 4 task from 5

---
+++ lesson3/hw3/script.js
@@ -0,0 +1,38 @@
+// 1
+// const a = 5;
+// const b = 10;
+
+// console.log(`addition`, a + b);
+
+// console.log(`subtraction`, a - b);
+
+// console.log(`division`, a / b);
+
+// console.log(`multiplication`, a * b);
+
+//3
+// const c = Number(prompt('Enter first value: '));
+// const d = Number(prompt('Enter second value: '));
+
+// if (c > d) {
+//     alert(`Yea, sure ${c} > ${d}`);
+// } else if (c < d) {
+//     alert(`Yea, sure ${c} < ${d}`);
+// } else {
+//     alert(`Yea, sure ${c} = ${d}`);
+// }
+
+// 4
+
+
+// 5
+// const visitorAge = Number(prompt('How old are you?'));
+// const isWithParents = confirm('Are you with parents?');
+
+// if (visitorAge >= 18 && visitorAge <= 60) {
+//     alert('Entrance to the roller coaster is available');
+// } else if (visitorAge >= 12 && visitorAge <= 80 && isWithParents) {
+//     alert('Entrance to the roller coaster open for you');
+// } else {
+//     alert('Entrance to the roller coaster is not available');
+// }
---
+++ lesson3/hw3/secondTask.js
@@ -0,0 +1,13 @@
+// 2
+true && true                        // true, (true);
+false || true                       // true, (true);
+true && 'false'                     // 'false', (true);
+0 && 1 || ' ' && 42                 // 0 || 42,   42, (true);
+null == undefined                   //true;
+'' == 0                             //true;
+1 === '1'                           //false
+NaN === NaN && true                 //NaN === true, (false);
+true || false && true || false      // true || false || false,     true || false   true,  (true);
+(true || false) && (true || false)  //true && true,     true,   (true);
+10 || 0 && 'dog' || ''              // 10 || 0 || '',    10 || '',  10,  (true);
+(10 || 0) && ('dog' || '')          // 10 && 'dog',   'dog',   (true);
