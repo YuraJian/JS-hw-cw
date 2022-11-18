18660544b71d221e5a1d406a87014ca00811d7bd
YuraJian <ur.duimja@gmail.com>
2022-11-18 23:43:12 +0200

    complete hw4

--- lesson4/hw4/script.js
+++ lesson4/hw4/script.js
@@ -17,6 +17,7 @@ function isEqual2(a, b) {
 console.log("isEqual2", isEqual2(5, 6));
 console.log("isEqual2", isEqual2(6, 6));
 
+// arrow func
 const isEqual3 = (a, b) => {
   return a === b ? true : false;
 };
@@ -34,6 +35,7 @@ console.log("getSum", getSum(5, 5));
 console.log("getSum", getSum(10, 5));
 console.log("getSum", getSum(15, 19));
 
+// arrow func
 const getSum2 = (a, b) => a + b;
 
 console.log("getSum2", getSum2(29, 20));
@@ -50,6 +52,7 @@ console.log("getExponentiation", getExponentiation(2));
 console.log("getExponentiation", getExponentiation(4));
 console.log("getExponentiation", getExponentiation(16));
 
+// arrow func
 const getExponentiation2 = (a, b = 2) => a ** b;
 
 console.log("getExponentiation2", getExponentiation2(256));
@@ -91,3 +94,71 @@ function getNum() {
 }
 
 getNum();
+
+// arrow func
+const getNum2 = () => {
+    const dayOfWeek = Number(prompt(
+        "Hello. If you want choose some day of week, choose one number of 1-7"
+      ));
+    
+      switch (dayOfWeek) {
+        case 1:
+          return console.log("Monday");
+    
+        case 2:
+          return console.log("Tuesday");
+    
+        case 3:
+          return console.log("Wednesday");
+    
+        case 4:
+          return console.log("Thursday");
+    
+        case 5:
+          return console.log("Friday");
+    
+        case 6:
+          return console.log("Saturday");
+    
+        case 7:
+          return console.log("Sunday");
+    
+        default:
+            return console.log('I do not know this day');
+      }
+}
+
+getNum2();
+
+// Task 5
+
+function getUseData() {
+    const userName = prompt('What is your name?');
+    const userAge = Number(prompt('How old are you?'));
+
+    if (!userName || !userAge || typeof userAge !== 'number') {
+        console.log('The data is incorrect!');
+    } else if (userAge > 30) {
+        console.log(`Good afternoon,`, userName);
+    } else {
+        console.log(`Hello,`, userName);
+    }
+};
+
+getUseData();
+
+// arrow func
+const getUseData2 = () => {
+    const userName = prompt('What is your name?');
+    const userAge = Number(prompt('How old are you?'));
+
+    if (!userName || !userAge || typeof userAge !== 'number') {
+        console.log('The data is incorrect!');
+    } else if (userAge > 30) {
+        console.log(`Good afternoon,`, userName);
+    } else {
+        console.log(`Hello,`, userName);
+    }
+};
+
+getUseData2();
